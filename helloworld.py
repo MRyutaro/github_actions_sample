af8b51e22947d1404745b1dc99cc9c2a1149d474
MATSUMOTO Ryutaro <81416251+MRyutaro@users.noreply.github.com>
2023-01-19 16:51:46 +0900

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,6 @@
-print("Hello Python!!!!!!!!!!!!!!!!!!!!!!!!")
+import datetime
+now = datetime.datetime.now()
 f = open("sample.txt", "w")
 f.write("書き込みました。ねねねね！！！！！！！")
+f.write(f"今の時間は{now}です")
 f.close()
